64ea3243d7533cecc719ff17d90e307cd8e95c2a
BurnBridges <163343287+BurnBridges@users.noreply.github.com>
2024-04-09 01:55:18 +0300

    Update app.js

--- app.js
+++ app.js
@@ -8,46 +8,28 @@ tg.MainButton.color = '#2cab37';
 let items = [];
 
 function toggleItem(btn, itemId, price) {
-    let item = items.find(i => i.id === itemId);
-    if (!item) {
-        let newItem = { id: itemId, price: price };
-        items.push(newItem);
+    let itemIndex = items.findIndex(item => item.id === itemId);
+    if (itemIndex === -1) {
+        items.push({ id: itemId, price: price });
         btn.classList.add('added-to-cart');
         btn.innerText = "Удалить из корзины";
-        let totalPrice = items.reduce((total, item) => total + item.price, 0);
-        if (totalPrice > 0) {
-            tg.MainButton.setText(`Общая цена товаров: ${totalPrice}`);
-            if (!tg.MainButton.isVisible) {
-                tg.MainButton.show();
-            }
-        } else {
-            tg.MainButton.hide();
-        }
     } else {
-        let index = items.indexOf(item);
-        items.splice(index, 1);
+        items.splice(itemIndex, 1);
         btn.classList.remove('added-to-cart');
         btn.innerText = "Добавить в корзину";
-        let totalPrice = items.reduce((total, item) => total + item.price, 0);
-        if (totalPrice > 0) {
-            tg.MainButton.setText(`Общая цена товаров: ${totalPrice}`);
-            if (!tg.MainButton.isVisible) {
-                tg.MainButton.show();
-            }
-        } else {
-            tg.MainButton.hide();
-        }
     }
-
+    updateTotalPrice();
 }
 
-Telegram.WebApp.onEvent('mainButtonClicked', function(){
-    let data = {
-        items: items,
-        totalPrice: calculateTotalPrice()
-    };
-    tg.sendData(JSON.stringify(data));
-});
+function updateTotalPrice() {
+    let totalPrice = calculateTotalPrice();
+    if (totalPrice > 0) {
+        tg.MainButton.setText(`Общая цена товаров: ${totalPrice}`);
+        tg.MainButton.show();
+    } else {
+        tg.MainButton.hide();
+    }
+}
 
 function calculateTotalPrice() {
     return items.reduce((total, item) => total + item.price, 0);
@@ -55,23 +37,23 @@ function calculateTotalPrice() {
 
 document.getElementById("btn1").addEventListener('click', function(){
     toggleItem(this, "item1" , 600);
-})
+});
 
 document.getElementById("btn2").addEventListener('click', function(){
     toggleItem(this, "item2" , 450);
+});
 
-})
 document.getElementById("btn3").addEventListener('click', function(){
     toggleItem(this, "item3" , 580);
+});
 
-})
 document.getElementById("btn4").addEventListener('click', function(){
     toggleItem(this, "item4" , 490);
+});
 
-})
 document.getElementById("btn5").addEventListener('click', function(){
     toggleItem(this, "item5" , 600);
-})
+});
 
 document.getElementById("btn6").addEventListener('click', function(){
     toggleItem(this, "item6" , 610);
