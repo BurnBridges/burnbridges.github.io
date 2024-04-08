fe50dc00013e2680566f79e17ea8577f316a7651
BurnBridges <163343287+BurnBridges@users.noreply.github.com>
2024-04-09 01:39:43 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,56 +1,78 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="style.css">
-	<title>Document</title>
-</head>
-<body>
-	<div class="container">
-		<div class="inner">
-			<div class="item" id="item1">
-				<img src="1.png" alt="" class="img">
-                <p>Стоимость: 600</p>
-				<button class="btn" id="btn1">Добавить в корзину</button>
-			</div>
+let tg = window.Telegram.WebApp;
 
-			<div class="item" id="item2">
-				<img src="1.png" alt="" class="img">
-                <p>Стоимость: 450</p>
-				<button class="btn" id="btn2">Добавить в корзину</button>
-			</div>
+tg.expand();
 
-			<div class="item" id="item3">
-				<img src="1.png" alt="" class="img">
-                <p>Стоимость: 580</p>
-				<button class="btn" id="btn3">Добавить в корзину</button>
-			</div>
+tg.MainButton.textColor = '#FFFFFF';
+tg.MainButton.color = '#2cab37';
 
-			<div class="item" id="item4">
-				<img src="1.png" alt="" class="img">
-                <p>Стоимость: 490</p>
-				<button class="btn" id="btn4">Добавить в корзину</button>
-			</div>
+let items = [];
 
-			<div class="item" id="item5">
-				<img src="1.png" alt="" class="img">
-                <p>Стоимость: 600</p>
-				<button class="btn" id="btn5">Добавить в корзину</button>
-			</div>
+function toggleItem(btn, itemId, price) {
+    let item = items.find(i => i.id === itemId);
+    if (!item) {
+        let newItem = { id: itemId, price: price };
+        items.push(newItem);
+        btn.classList.add('added-to-cart');
+        btn.innerText = "Удалить из корзины";
+        let totalPrice = items.reduce((total, item) => total + item.price, 0);
+        if (totalPrice > 0) {
+            tg.MainButton.setText(`Общая цена товаров: ${totalPrice}`);
+            if (!tg.MainButton.isVisible) {
+                tg.MainButton.show();
+            }
+        } else {
+            tg.MainButton.hide();
+        }
+    } else {
+        let index = items.indexOf(item);
+        items.splice(index, 1);
+        btn.classList.remove('added-to-cart');
+        btn.innerText = "Добавить в корзину";
+        let totalPrice = items.reduce((total, item) => total + item.prirce, 0);
+        if (totalPrice > 0) {
+            tg.MainButton.setText(`Общая цена товаров: ${totalPrice}`);
+            if (!tg.MainButton.isVisible) {
+                tg.MainButton.show();
+            }
+        } else {
+            tg.MainButton.hide();
+        }
+    }
 
-			<div class="item" id="item6">
-				<img src="1.png" alt="" class="img">
-                <p>Стоимость: 610</p>
-				<button class="btn" id="btn6">Добавить в корзину</button>
-			</div>
-		</div>
-	</div>
-	<div class="usercard" id="usercard">
-		
-	</div>
+}
 
-	<script src="https://telegram.org/js/telegram-web-app.js"></script>
-	<script src="app.js"></script>
-</body>
-</html>
+Telegram.WebApp.onEvent('mainButtonClicked', function(){
+    let data = {
+        items: items,
+        totalPrice: calculateTotalPrice()
+    };
+    tg.sendData(JSON.stringify(data));
+});
+
+function calculateTotalPrice() {
+    return items.reduce((total, item) => total + item.price, 0);
+}
+
+document.getElementById("btn1").addEventListener('click', function(){
+    toggleItem(this, "item1" , 600);
+})
+
+document.getElementById("btn2").addEventListener('click', function(){
+    toggleItem(this, "item2" , 450);
+
+})
+document.getElementById("btn3").addEventListener('click', function(){
+    toggleItem(this, "item3" , 580);
+
+})
+document.getElementById("btn4").addEventListener('click', function(){
+    toggleItem(this, "item4" , 490);
+
+})
+document.getElementById("btn5").addEventListener('click', function(){
+    toggleItem(this, "item5" , 600);
+})
+
+document.getElementById("btn6").addEventListener('click', function(){
+    toggleItem(this, "item6" , 610);
+});
