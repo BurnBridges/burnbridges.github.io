c4c27cc9af8bc3f654a2a8ba8c7d3058eb04c21d
BurnBridges <163343287+BurnBridges@users.noreply.github.com>
2024-04-13 22:41:59 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,83 +1,98 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Document</title>
-</head>
-<body>
-    <div class="center">
-        <div class="container">
-            <div class="inner">
-                <div class="item" id="item1">
-                    <img src="burger.png" alt="" class="img" id="burger-img">
-                    <p>burger - <b>600Р</b></p>
-                    <div class="quantity-control">
-                        <button class="btn" id="minusBtn1" onclick="decrementQuantity('quantity1')">-</button>
-                        <span id="quantity1">0</span>
-                        <button class="btn" id="btn1" onclick="incrementQuantity('quantity1'); playAnimation('burger-img', 'burger.gif');">+</button>
-                    </div>
-                </div>
-                
-                <div class="item" id="item2">
-                    <img src="pizza.png" alt="" class="img" id="pizza-img">
-                    <p>pizza - <b>450Р</b></p>
-                    <div class="quantity-control">
-                        <button class="btn" id="minusBtn2" onclick="decrementQuantity('quantity2')">-</button>
-                        <span id="quantity2">0</span>
-                        <button class="btn" id="btn2" onclick="incrementQuantity('quantity2'); playAnimation('pizza-img', 'pizza-animation.gif');">+</button>
-                    </div>
-                </div>
-                
-                <div class="item" id="item3">
-                    <img src="wok.png" alt="" class="img" id="wok-img">
-                    <p>onigiri - <b>580Р</b></p>
-                    <div class="quantity-control">
-                        <button class="btn" id="minusBtn3" onclick="decrementQuantity('quantity3')">-</button>
-                        <span id="quantity3">0</span>
-                        <button class="btn" id="btn3" onclick="incrementQuantity('quantity3'); playAnimation('wok-img', 'wok-animation.gif');">+</button>
-                    </div>
-                </div>
+let tg = window.Telegram.WebApp;
 
-                <div class="item" id="item4">
-                    <img src="onigiri.png" alt="" class="img" id="onigiri-img">
-                    <p>rice - <b>490Р</b></p>
-                    <div class="quantity-control">
-                        <button class="btn" id="minusBtn4" onclick="decrementQuantity('quantity4')">-</button>
-                        <span id="quantity4">0</span>
-                        <button class="btn" id="btn4" onclick="incrementQuantity('quantity4'); playAnimation('onigiri-img', 'onigiri-animation.gif');">+</button>
-                    </div>
-                </div>
+tg.expand();
 
-                <div class="item" id="item5">
-                    <img src="roll.png" alt="" class="img" id="roll-img">
-                    <p>roll - <b>600Р</b></p>
-                    <div class="quantity-control">
-                        <button class="btn" id="minusBtn5" onclick="decrementQuantity('quantity5')">-</button>
-                        <span id="quantity5">0</span>
-                        <button class="btn" id="btn5" onclick="incrementQuantity('quantity5'); playAnimation('roll-img', 'roll-animation.gif');">+</button>
-                    </div>
-                </div>
+tg.MainButton.textColor = '#FFFFFF';
+tg.MainButton.color = '#2cab37';
 
-                <div class="item" id="item6">
-                    <img src="juice.png" alt="" class="img" id="juice-img">
-                    <p>juice - <b>610Р</b></p>
-                    <div class="quantity-control">
-                        <button class="btn" id="minusBtn6" onclick="decrementQuantity('quantity6')">-</button>
-                        <span id="quantity6">0</span>
-                        <button class="btn" id="btn6" onclick="incrementQuantity('quantity6'); playAnimation('juice-img', 'juice-animation.gif');">+</button>
-                    </div>
-                </div>
-                <div>
-                    <label for="address">Введите ваш адрес:</label><br>
-                    <input type="text" id="address" name="address"><br><br>
-                </div>
-            </div>
-        </div>
-    </div>
+let items = [];
 
-    <script src="https://telegram.org/js/telegram-web-app.js"></script>
-    <script src="app.js"></script>
-</body>
-</html>
+function toggleItem(btn, itemId, price) {
+    let itemIndex = items.findIndex(item => item.id === itemId);
+    if (itemIndex === -1) {
+        // Если товар не найден в корзине, добавляем его с количеством 1
+        items.push({ id: itemId, price: price, quantity: 1 });
+    } else {
+        // Если товар найден в корзине, увеличиваем его количество
+        items[itemIndex].quantity++;
+    }
+    updateTotalPrice();
+}
+
+function updateTotalPrice() {
+    let totalPrice = calculateTotalPrice();
+    if (totalPrice > 0) {
+        tg.MainButton.setText(`View order: ${totalPrice}`);
+        tg.MainButton.show();
+    } else {
+        tg.MainButton.hide();
+    }
+}
+
+Telegram.WebApp.onEvent('mainButtonClicked', function(){
+    let data = {
+        items: items,
+        totalPrice: calculateTotalPrice()
+    };
+    tg.sendData(JSON.stringify(data));
+})
+
+function calculateTotalPrice() {
+    return items.reduce((total, item) => total + item.price * parseInt(document.getElementById(item.id).querySelector('.quantity-control span').innerText), 0);
+}
+
+document.getElementById("btn1").addEventListener('click', function(){
+    toggleItem(this, "item1" , 600);
+});
+
+document.getElementById("btn2").addEventListener('click', function(){
+    toggleItem(this, "item2" , 450);
+});
+
+document.getElementById("btn3").addEventListener('click', function(){
+    toggleItem(this, "item3" , 580);
+});
+
+document.getElementById("btn4").addEventListener('click', function(){
+    toggleItem(this, "item4" , 490);
+});
+
+document.getElementById("btn5").addEventListener('click', function(){
+    toggleItem(this, "item5" , 600);
+});
+
+document.getElementById("btn6").addEventListener('click', function(){
+    toggleItem(this, "item6" , 610);
+});
+
+function playAnimation(imgId, animationSrc) {
+    // Находим элемент изображения
+    const img = document.getElementById(imgId);
+
+    // Сохраняем текущий путь изображения для восстановления
+    const originalSrc = img.src;
+
+    // Заменяем статическое изображение анимированным GIF
+    img.src = animationSrc;
+
+    // Запускаем таймер для возврата к статическому изображению через 3 секунды (или сколько вам нужно)
+    setTimeout(function() {
+        img.src = originalSrc; // Возвращаем изображение к первоначальному статусу
+    }, 3000); // 3000 миллисекунд = 3 секунды
+}
+
+function incrementQuantity(quantityId) {
+    let quantityElement = document.getElementById(quantityId);
+    let quantity = parseInt(quantityElement.innerText);
+    quantityElement.innerText = quantity + 1;
+    updateTotalPrice(); // Обновляем общую сумму заказа при увеличении количества
+}
+
+function decrementQuantity(quantityId) {
+    let quantityElement = document.getElementById(quantityId);
+    let quantity = parseInt(quantityElement.innerText);
+    if (quantity > 0) {
+        quantityElement.innerText = quantity - 1;
+        updateTotalPrice(); // Обновляем общую сумму заказа при уменьшении количества
+    }
+}
