41f26056466f2593d1458a88cd21a1c0107853be
BurnBridges <163343287+BurnBridges@users.noreply.github.com>
2024-04-14 00:28:40 +0300

    Update app.js

--- app.js
+++ app.js
@@ -102,12 +102,6 @@ function decrementQuantity(quantityId) {
 // Добавляем обработчик события для кнопки "Готово"
 document.getElementById("submitAddressBtn").addEventListener('click', function() {
     var address = document.getElementById("address").value;
-    var addressField = document.getElementById("address");
-    var addressText = document.createElement("p");
-    addressText.textContent = "Адрес доставки: " + address;
-    addressField.parentNode.replaceChild(addressText, addressField);
-    
-    // Скрыть текст "Введите ваш адрес"
-    var placeholderText = document.querySelector('label[for="address"]');
-    placeholderText.style.display = 'none';
+    // Отправляем адрес на сервер вместе с данными о заказе
+    sendDataWithAddress(address);
 });
